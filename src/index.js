7659b752aa23113d9ce3475a3ac0257d76277ca3
Marcos Lima <marcoslima749@gmail.com>
2021-08-09 01:05:04 -0300

    acomodado un poco el css. inicializada la funcionalidad. captura la pantalla pero hay que ordenar todo.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 //console.log("hola, what's happening, no cap in my caption");
+
+
+
+const ESTADO = {
+    GRABANDO : "GRABANDO",
+    DETENIDO : "DETENIDO",
+    PAUSADO : "PAUSADO"
+}
+
+const URL_ICONO = {
+    GRABAR : "/public/icons/record-fill.svg",
+    PAUSAR : "/public/icons/pause-circle.svg",
+    DETENER : "/public/icons/stop-circle.svg"
+}
+
+
+let captura = document.getElementById("captura");
+
+let captureOptions = {
+    video : {
+        cursor : 'always'
+    },
+    audio: false
+}
+
+const startCapture = async () => {
+
+    //getUserMedia va a llamar a las cámaras
+    //getDisplayMedia va a capturar la pantalla
+    
+    try {
+        captura.srcObject = await navigator.mediaDevices.getDisplayMedia(captureOptions);
+        
+    } catch (err) {
+        console.log("error: " + err );
+    }
+    
+};
+
+const stopCapture = async () => {
+    let tracks = captura.srcObject.getTracks();
+    tracks.forEach(track => {
+        track.stop();
+    });
+    captura.srcObject = null;
+};
+
+const pauseCapture = async () => {
+    
+};
+
+const botonAccion = document.getElementById("botonAccion");
+botonAccion.addEventListener("click", startCapture);
